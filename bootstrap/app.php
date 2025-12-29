e72f71b639dfd0082a39104985939cd3a6ed1b04
emmanuelreydelmercado-rgb <mariselvam@reydelmercado.com>
2025-12-29 18:43:19 +0530

    Fix: Add TrustProxies middleware to fix HTTPS URL generation behind Render proxy

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -11,6 +11,9 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
+        // Trust Render's proxy for HTTPS detection
+        $middleware->trustProxies(at: '*');
+        
         $middleware->alias([
             'admin' => \App\Http\Middleware\AdminMiddleware::class,
             'admin.email' => \App\Http\Middleware\AdminEmailMiddleware::class,
