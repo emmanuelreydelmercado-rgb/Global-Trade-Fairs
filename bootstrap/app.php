854c55791f93476ce0218ee383f936974c7ce8ac
Reydel Mercado <reydel@globaltrade.com>
2026-03-03 17:52:33 +0530

    feat: add Laravel Telescope and Sentry error monitoring

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -27,6 +27,6 @@ return Application::configure(basePath: dirname(__DIR__))
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
-        //
+        \Sentry\Laravel\Integration::handles($exceptions);
     })
     ->create();
